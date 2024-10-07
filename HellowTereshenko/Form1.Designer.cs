0c0721ac3017702f99c42b9c11cc85ce3bd54b76
Темур Норкулов <temur-norkulov@mail.ru>
2024-10-07 16:02:56 +0700

    Я сделал негру больно

--- HellowTereshenko/Form1.Designer.cs
+++ HellowTereshenko/Form1.Designer.cs
@@ -28,33 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.button1 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(629, 329);
+            this.button1.Location = new System.Drawing.Point(889, 389);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(123, 34);
             this.button1.TabIndex = 0;
             this.button1.Text = "Запуск";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(23, 21);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(474, 315);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(786, 183);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.TabIndex = 2;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1051, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.chart1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- HellowTereshenko/Form1.cs
+++ HellowTereshenko/Form1.cs
@@ -7,14 +7,107 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace HellowTereshenko
 {
     public partial class Form1 : Form
     {
+        // Параметры для симуляции
+        private int[] stepsPerSubdialog = { 5, 7 }; // Шаги в подтемах
+        private double[][] errorProbabilities = { new double[] { 0.1, 0.05, 0.1, 0.08, 0.07 },
+                                                  new double[] { 0.2, 0.15, 0.1, 0.12, 0.08, 0.05, 0.07 } }; // Вероятности ошибок
+        private double[] executionTimes = { 2.0, 3.0 }; // Время выполнения шагов
+        private double[] subdialogProbabilities = { 0.6, 0.4 }; // Вероятности выбора подтем
+
+        private int numSimulations = 1000;
+        private Random random = new Random();
+
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+
+        // Моделирование выполнения одной подтемы
+        private double SimulateSubdialog(int subdialogIndex)
+        {
+            double totalTime = 0;
+            int step = 0;
+
+            while (step < stepsPerSubdialog[subdialogIndex])
+            {
+                // Время выполнения шага
+                double time = -Math.Log(random.NextDouble()) * executionTimes[subdialogIndex];
+                totalTime += time;
+
+                // Проверка на ошибку
+                if (random.NextDouble() < errorProbabilities[subdialogIndex][step])
+                {
+                    // Возврат на предыдущий шаг
+                    if (step > 0) step--;
+                }
+                else
+                {
+                    // Переход к следующему шагу
+                    step++;
+                }
+            }
+
+            return totalTime;
+        }
+
+        // Моделирование диалога
+        private double SimulateDialog()
+        {
+            // Выбор подтемы
+            int subdialogChoice = random.NextDouble() < subdialogProbabilities[0] ? 0 : 1;
+
+            // Выполнение подтемы
+            return SimulateSubdialog(subdialogChoice);
+        }
+
+        // Запуск симуляции
+        private void RunSimulation_Click(object sender, EventArgs e)
+        {
+            List<double> totalTimes = new List<double>();
+
+            // Выполнение симуляций
+            for (int i = 0; i < numSimulations; i++)
+            {
+                totalTimes.Add(SimulateDialog());
+            }
+
+            // Вывод результатов
+            double averageTime = totalTimes.Average();
+            resultTextBox.Text = $"Среднее время выполнения задачи: {averageTime:F2} секунд";
+
+            // Построение гистограммы
+            PlotHistogram(totalTimes);
+        }
+        private void PlotHistogram(List<double> data)
+        {
+            histogramChart.Series.Clear();
+            histogramChart.Titles.Clear();
+
+            histogramChart.Titles.Add("Гистограмма времени выполнения");
+            var series = new Series
+            {
+                Name = "Execution Time",
+                ChartType = SeriesChartType.Column
+            };
+            histogramChart.Series.Add(series);
+
+            var dataGrouped = data.GroupBy(d => Math.Floor(d)).ToDictionary(g => g.Key, g => g.Count());
+            foreach (var entry in dataGrouped)
+            {
+                series.Points.AddXY(entry.Key, entry.Value);
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
